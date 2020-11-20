a5f87104fc038e160635f7123c597fe546351248
nidhal-lourimi <nidhallourimi@gmail.com>
2020-11-20 14:07:32 +0100

    final commit

---
+++ dogs/src/main/java/com/udacity/dogs/DogsApplication.java
@@ -0,0 +1,15 @@
+package com.udacity.dogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsApplication.class, args);
+	}
+
+}
